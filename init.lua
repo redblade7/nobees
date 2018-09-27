59f73c9cda7e7305c159583d8668e03651ac1fae
I am not me <impeccabletiming@gmail.com>
2018-09-27 18:29:25 -0400

    Initial commit

---
+++ init.lua
@@ -0,0 +1,4 @@
+minetest.register_alias("mobs:beehive", "default:tree")
+minetest.register_alias("bees:hives_artificial", "default:tree")
+minetest.register_alias("bees:extractor", "default:tree")
+minetest.register_alias("bees:hive_wild", "default:tree")
