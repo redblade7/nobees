fc560e1b1de19be0a4d3b8527cb2d9db6ecf6bcb
I am not me <impeccabletiming@gmail.com>
2018-09-27 19:42:07 -0400

    typo

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
-minetest.register_alias("mobs:beehive", "default:tree")
-minetest.register_alias("bees:hives_artificial", "default:tree")
+minetest.register_alias("bees:hive_artificial", "default:tree")
+minetest.register_alias("bees:hive_artificial_inhabited", "default:tree")
 minetest.register_alias("bees:extractor", "default:tree")
 minetest.register_alias("bees:hive_wild", "default:tree")
+
