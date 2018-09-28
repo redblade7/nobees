618477b884429cf8f60e2e1edba7f967a7f54c3b
I am not me <impeccabletiming@gmail.com>
2018-09-27 20:18:17 -0400

    Contribution from redblade7

--- init.lua
+++ init.lua
@@ -1,5 +1,23 @@
+####original by LDH
+minetest.register_alias("mobs:beehive", "default:tree")
 minetest.register_alias("bees:hive_artificial", "default:tree")
-minetest.register_alias("bees:hive_artificial_inhabited", "default:tree")
 minetest.register_alias("bees:extractor", "default:tree")
 minetest.register_alias("bees:hive_wild", "default:tree")
-
+####others by redblade7
+minetest.register_alias("bees:frame_empty", "default:stick")
+minetest.register_alias("bees:frame_full", "default:stick")
+minetest.register_alias("bees:wax", "default:stick")
+minetest.register_alias("bees:smoker", "default:stick")
+minetest.register_alias("bees:grafting_tool", "default:stick")
+minetest.register_alias("bees:hive_artificial_inhabited","default:tree")
+minetest.register_alias("bees:hive_industrial", "default:tree")
+minetest.register_alias("bees:bees", "default:tree")
+####by redblade7 - requires mobs and mobs_animal
+minetest.register_alias("bees:colony", "mobs_animal:bee")
+minetest.register_alias("bees:honey_bottle", "mobs_animal:honey_block")
+minetest.register_alias("bees:honey_comb", "mobs_animal:honey_block")
+####compatibility with original bees mod - added by redblade7
+minetest.register_alias("bees:honey_extractor", "default:tree")
+minetest.register_alias("bees:hive", "default:tree")
+minetest.register_alias("bees:queen", "mobs_animal:bee")
+minetest.register_alias("bees:bottle_honey", "mobs_animal:honey_block")
